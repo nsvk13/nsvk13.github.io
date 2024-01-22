a96395f47e67a47c09e3e814829033a1ec2c1616
nsvk13 <nsvkdev@gmail.com>
2024-01-22 06:00:16 +0300

    chore: pizdec

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+function openTab(tabId) {
+    var tabs = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+    document.getElementById(tabId).style.display = "block";
+}
